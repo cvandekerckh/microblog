a65f6b872e6cd70d0394c153f4a90e59d666811e
Corentin Vande Kerckhove <corentin@delphia.com>
2020-04-04 11:19:27 -0400

    add config and main script

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you will never guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+            'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['corentinvdk@gmail.com'] # https://myaccount.google.com/lesssecureapps unlock
+    POSTS_PER_PAGE = 3
+    LANGUAGES = ["en", "fr"]
+
+# Environment variables
+# export MAIL_SERVER=smtp.googlemail.com
+# export MAIL_PORT=587
+# export MAIL_USE_TLS=1
+# export MAIL_USERNAME=corentinvdk@gmail.com
+# export MAIL_PASSWORD=<your-gmail-password>
