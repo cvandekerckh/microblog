83f1d0e121e26970da46e4a4abd9a3a153f6aa5c
cvandekerckh <corentinvdk@gmail.com>
2020-04-14 17:32:50 -0400

    prepare for authentification

--- app/models.py
+++ app/models.py
@@ -12,6 +12,9 @@ from time import time
 import redis
 import rq
 from flask import url_for
+import base64
+from datetime import timedelta
+import os
 
 
 
@@ -112,7 +115,8 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
                                     lazy='dynamic')
 
     tasks = db.relationship('Task', backref='user', lazy='dynamic')
-
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -215,6 +219,26 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
                 setattr(self, field, data[field])
         if new_user and 'password' in data:
             self.set_password(data['password'])
+
+
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+
+    @staticmethod
+    def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return user
 
 
 @login.user_loader
